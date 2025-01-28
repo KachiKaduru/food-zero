220978580fc7f8c8b914dfc89a6d1ac04d9098d4
Kachii <kachikaduru@gmail.com>
2025-01-28 23:33:50 +0100

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import "@/app/_styles/globals.css";
 
+const metadata = {
+  template: {
+    title: "%s | Food Zero",
+    default: "Welcome | Food Zero",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html>
